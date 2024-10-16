cac3a06e9271a0f52df6029470eacf762c26cec1
Tweber663 <tweber.1995@hotmail.com>
2024-10-16 20:12:27 +0200

    damage mechanism working

--- app.js
+++ app.js
@@ -3,29 +3,69 @@ const app = Vue.createApp({
         return {
             playerHealth: 100, 
             monsterHealth: 100,  
+            currentRound: 0, 
+            gameFinished: false, 
         }
     }, 
+    watch: {
+        currentRound() {
+            if (this.playerHealth <= 0 || this.monsterHealth <= 0) this.gameFinished = true;
+        }
+    },
     computed: {
         updatePlayerHp() {
             return this.playerHealth <= 0?  {width: `${0}%`} : {width: `${this.playerHealth}%`} 
         }, 
         updateMonsterHp() {
-            // return {width: `${this.monsterHealth}%`};
             return this.monsterHealth <= 0?  {width: `${0}%`} : {width: `${this.monsterHealth}%`} 
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0? true : false;
+        },
+        gameOverResults() {
+            if (this.playerHealth <= 0 && this.monsterHealth <= 0) {
+                return 'Draw!'
+            } else if (this.playerHealth <= 0) {
+                return 'Monster Win!'
+            } else {
+                return 'Player Win!'; 
+            }
+        },
+        showResults() {
+            return this.gameFinished === false? false : true;
         }
     },
     methods: {
         attackMonster() {
+           this.currentRound++;
            const attackValue = randomDamage(5, 20)
            this.monsterHealth -= attackValue; 
-           console.log('Played Attacked! Damaged caused to monster:', attackValue);
            this.attackPlayer(); 
         }, 
+        specialAttackMonster() {
+            this.currentRound++; 
+            const attackValue= randomDamage(10, 25); 
+            this.monsterHealth -= attackValue; 
+
+        },
         attackPlayer() {
            const attackValue = randomDamage(10, 30); 
            this.playerHealth -= attackValue; 
-           console.log('Monster attacked back! Damaged to player:', attackValue);
+        }, 
+        playerHeal() {
+            this.currentRound++; 
+            const healingValue = randomDamage(15, 35);
+            console.log(this.playerHealth + healingValue); 
+            this.playerHealth + healingValue >= 100? this.playerHealth = 100 : this.playerHealth += healingValue;
+            this.attackPlayer(); 
+        }, 
+        startNewGame() {
+            this.playerHealth = 100; 
+            this.monsterHealth = 100; 
+            this.currentRound = 0; 
+            this.gameFinished = false; 
         }
+
     }, 
 });
 
